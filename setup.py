74cb35c1f9af875426a2f9524ddf1941a771b4d0
Cyrille Doux <cdoux@sas.upenn.edu>
2019-02-26 17:41:34 -0800

    Actually adds setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='harmony',
+      version='0.1',
+      description='Tools to compute angular power spectra of different cosmological observables from either catalogs or maps using NaMaster, Healpy, etc...',
+      author='Cyrille Doux',
+      author_email='cdoux@sas.upenn.edu',
+      url='https://github.com/xuod/harmony',
+      packages=['harmony'],
+     )
