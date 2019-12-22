ec5a7f21373b576d10f5626117ad5186d0f9ab04
Cyrille Doux <cdoux@sas.upenn.edu>
2019-12-22 13:27:35 +0100

    Add keyword to use same path for evrything in config.

--- harmony/config.py
+++ harmony/config.py
@@ -1,7 +1,11 @@
 
 class Config(object):
-    def __init__(self, name='name', path_maps='../maps', path_figures='../figures', path_output='../output'):
+    def __init__(self, name='name', all_paths=None, path_maps='../maps', path_figures='../figures', path_output='../output'):
         self.name = name
+        if all_paths is not None:
+            self.path_maps = all_paths
+            self.path_figures = all_paths
+            self.path_output = all_paths
         self.path_maps = path_maps
         self.path_figures = path_figures
         self.path_output = path_output
