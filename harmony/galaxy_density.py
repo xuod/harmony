Fix imports, add path_all, rotation of galaxy_density catalog, weights for galaxy_shear, save cls with ext.

harmony/galaxy_density.py
@@ -1,6 +1,13 @@
-from .observable import *
+from .observable import Observable
+from .utils import hpunseen2zero
 import numba
-
+import twopoint
+from astropy.io import fits
+import os
+import healpy as hp
+import twopoint
+import numpy as np
+import castor as ca
 
 class Galaxy(Observable):
     def __init__(self, config, nside, mode, nzbins, mask_mode, data_dir='../data', mask_dir='../masks', get_count_weights_from=None, dry_run=False, density_convention=2, completeness_cut=0.8, *args, **kwargs):
@@ -126,13 +133,20 @@ class Galaxy(Observable):
     #         comp[comp == hp.UNSEEN] = 0.0
     #         self.maps[ibin]['completeness'] = comp
 
-    def make_maps(self, save=True):
+    def make_maps(self, save=True, rotator=None):
         for ibin in self.prog(self.zbins, desc='Galaxy.make_maps'):
             cat = self.cats[ibin]
 
+            if rotator is None:
+                ra = cat['ra']
+                dec = cat['dec']
+            else:
+                theta, phi = rotator(ca.cosmo.radec2thetaphi(cat['ra'], cat['dec']))
+                ra, dec = ca.cosmo.thetaphi2radec(theta, phi)
+
             # get_count_weights_from in [None, 'mask', 'catalog']
             if self.get_count_weights_from is None:
-                _, count, _ = ca.cosmo.make_healpix_map(cat['ra'], cat['dec'], None, self.nside,
+                _, count, _ = ca.cosmo.make_healpix_map(ra, dec, None, self.nside,
                                             mask=None,
                                             weight=None,
                                             fill_UNSEEN=False, return_extra=False)
@@ -141,7 +155,7 @@ class Galaxy(Observable):
 
             elif self.get_count_weights_from=='mask':
                 cat = self.cats[ibin]
-                ipix = hp.ang2pix(self.nside, (90-cat['dec'])*np.pi/180.0, cat['ra']*np.pi/180.0)
+                ipix = hp.ang2pix(self.nside, (90.-dec)*np.pi/180.0, ra*np.pi/180.0)
                 weights = cat['weights_map'][ipix]
                 w = weights>0.
                 count_w, count, _ = ca.cosmo.make_healpix_map(None, None,
@@ -153,7 +167,7 @@ class Galaxy(Observable):
                 density = ca.cosmo.count2density(count_w[0], mask=self.masks[ibin], completeness=self.maps[ibin]['completeness'], density_convention=self.density_convention)
 
             else: #self.get_count_weights_from=='catalog'
-                count_w, count, _ = ca.cosmo.make_healpix_map(cat['ra'], cat['dec'],
+                count_w, count, _ = ca.cosmo.make_healpix_map(ra, dec,
                                             [np.ones_like(cat['dec'])], self.nside,
                                             mask=None,
                                             weight=[cat['weight']],
@@ -255,7 +269,7 @@ class Galaxy(Observable):
         titles = {}
         ylabels = {}
 
-        idx_EB = [0, 1, 3]
+        # idx_EB = [0, 1, 3]
 
         for i, zbin in enumerate(self.zbins):
             k = (0,i)

harmony/galaxy_shear.py
@@ -1,15 +1,22 @@
-from .observable import *
-from .utils import *
-import sys
+from .observable import Observable
+from .utils import prog, hpunseen2zero
+import os, sys
 import numba
 import random
+import numpy as np
+import twopoint
+from astropy.io import fits
+import numpy as np
+import castor as ca
+import pymaster as nmt
+import healpy as hp
 
 TWOPI = 2. * np.pi
 
 class Shear(Observable):
     def __init__(self, config, nside, mode, nzbins, mask_mode, data_dir='../data', dry_run=False, *args, **kwargs):
         self.obs_name = 'galaxy_shear'
-        self.map_names = ['count', 'e1', 'e2']
+        self.map_names = ['count', 'e1', 'e2', 'weighted_count']
 
         super(Shear, self).__init__(config, nside, mode, nzbins, self.obs_name, self.map_names, *args, **kwargs)
 
@@ -31,42 +38,45 @@ class Shear(Observable):
 
         if not dry_run:
             if mode.startswith('buzzard'):
-                self._init_buzzard()
+                # self._init_buzzard()
+                raise NotImplementedError
             elif mode=='data_sub' or mode=='mastercat':
-                self._init_data()
+                self._init_data(kwargs.get('use_weights', True))
             elif mode=='mock':
-                self._init_mock()
+                self._init_mock(kwargs.get('use_weights', True))
+                # raise NotImplementedError
             elif mode=='full':
-                self._init_full(kwargs['filename_template'], kwargs['dict'], kwargs['flip_e2'], kwargs.get('single_file', False), kwargs.get('ext_template', 'zbin_{}'), kwargs.get('ipix_instead_of_radec', False))
+                self._init_full(kwargs['filename_template'], kwargs['dict'], kwargs['flip_e2'], kwargs.get('single_file', False), kwargs.get('ext_template', 'zbin_{}'), kwargs.get('ipix_instead_of_radec', False), kwargs.get('use_weights', True))
             elif mode=='tables':
-                self._init_tables(kwargs['tables'], kwargs['dict'], kwargs['flip_e2'], kwargs.get('ext_template', 'zbin_{}', kwargs.get('ipix_instead_of_radec', False)))
+                self._init_tables(kwargs['tables'], kwargs['dict'], kwargs['flip_e2'], kwargs.get('ext_template', 'zbin_{}', kwargs.get('ipix_instead_of_radec', False)), kwargs.get('use_weights', True))
             elif mode=='flask':
-                self._init_flask(kwargs['isim'], kwargs['cookie'])
+                # self._init_flask(kwargs['isim'], kwargs['cookie'])
+                raise NotImplementedError
             elif mode=='psf' or 'dry':
                 pass
             else:
                 raise ValueError("Given `mode` argument ({}) is not correct.".format(mode))
 
-    def _init_buzzard(self):
-        # self.zlims = [(.2, .43), (.43,.63), (.64,.9), (.9, 1.3), (.2,1.3)][:self.nzbins]
-        for ibin in self.prog(self.zbins):
-            filename = os.path.join(self.data_dir, "Niall_WL_y3_bin_{}.fits".format(ibin+1))
-            _cat = fits.open(filename)[1].data
+    # def _init_buzzard(self):
+    #     # self.zlims = [(.2, .43), (.43,.63), (.64,.9), (.9, 1.3), (.2,1.3)][:self.nzbins]
+    #     for ibin in self.prog(self.zbins):
+    #         filename = os.path.join(self.data_dir, "Niall_WL_y3_bin_{}.fits".format(ibin+1))
+    #         _cat = fits.open(filename)[1].data
 
-            cat = {}
-            cat['ra'] = _cat['RA']
-            cat['dec'] = _cat['DEC']
+    #         cat = {}
+    #         cat['ra'] = _cat['RA']
+    #         cat['dec'] = _cat['DEC']
 
-            if self.mode == 'buzzard':
-                cat['e1'] = _cat['E1']
-                cat['e2'] = _cat['E2']
-            if self.mode == 'buzzard_truth':
-                cat['e1'] = _cat['G1']
-                cat['e2'] = _cat['G2']
+    #         if self.mode == 'buzzard':
+    #             cat['e1'] = _cat['E1']
+    #             cat['e2'] = _cat['E2']
+    #         if self.mode == 'buzzard_truth':
+    #             cat['e1'] = _cat['G1']
+    #             cat['e2'] = _cat['G2']
 
-            self.cats[ibin] = cat
+    #         self.cats[ibin] = cat
 
-    def _init_data(self):
+    def _init_data(self, use_weights=True):
         for ibin in self.prog(self.zbins):
             filename = os.path.join(self.data_dir, "source_s{}.fits".format(ibin+1))
             _cat = fits.open(filename)[1].data
@@ -78,9 +88,15 @@ class Shear(Observable):
             cat['e1'] = _cat['g1']
             cat['e2'] = -1.0 * _cat['g2']
 
+            if use_weights:
+                    cat['weight'] = _cat['weight'].astype(float)
+            else:
+                # print("Data file does not have weight column")
+                cat['weight'] = None
+
             self.cats[ibin] = cat
 
-    def _init_mock(self):
+    def _init_mock(self, use_weights=True):
         for ibin in self.prog(self.zbins):
             filename = os.path.join(self.data_dir, "source_s{}.fits".format(ibin+1))
             _cat = fits.open(filename)[1].data
@@ -92,9 +108,15 @@ class Shear(Observable):
             cat['e1'] = _cat['e1']
             cat['e2'] = _cat['e2']
 
+            if use_weights:
+                    cat['weight'] = _cat['weight'].astype(float)
+            else:
+                # print("Data file does not have weight column")
+                cat['weight'] = None
+
             self.cats[ibin] = cat
 
-    def _init_full(self, filename_template, dict, flip_e2, single_file=False, ext_template='zbin_{}', ipix_instead_of_radec=False):
+    def _init_full(self, filename_template, dict, flip_e2, single_file=False, ext_template='zbin_{}', ipix_instead_of_radec=False, use_weights=True):
         if single_file:
             full_cat = fits.open(os.path.join(self.data_dir, filename_template))
         if ipix_instead_of_radec:
@@ -113,6 +135,11 @@ class Shear(Observable):
                 cat['ra'] = _cat[dict['ra']]
                 cat['dec'] = _cat[dict['dec']]
 
+            if use_weights:
+                cat['weight'] = _cat[dict['weight']]
+            else:
+                cat['weight'] = None
+            
             cat['e1'] = _cat[dict['e1']]
             cat['e2'] = _cat[dict['e2']]
 
@@ -121,7 +148,7 @@ class Shear(Observable):
 
             self.cats[ibin] = cat    
 
-    def _init_tables(self, tables, dict, flip_e2=False, ext_template='zbin_{}', ipix_instead_of_radec=False):
+    def _init_tables(self, tables, dict, flip_e2=False, ext_template='zbin_{}', ipix_instead_of_radec=False, use_weights=True):
         is_list = isinstance(tables, list)
         if ipix_instead_of_radec:
             self.ipix = {}
@@ -138,6 +165,11 @@ class Shear(Observable):
                 cat['ra'] = _cat[dict['ra']]
                 cat['dec'] = _cat[dict['dec']]
 
+            if use_weights:
+                cat['weight'] = _cat[dict['weight']]
+            else:
+                cat['weight'] = None
+            
             cat['e1'] = _cat[dict['e1']]
             cat['e2'] = _cat[dict['e2']]
 
@@ -146,30 +178,69 @@ class Shear(Observable):
 
             self.cats[ibin] = cat
 
-    def _init_flask(self, isim, cookie):
-        for ibin in self.prog(self.zbins):
-            filename = os.path.join(self.data_dir, 'src-cat_s{}_z{}_ck{}.fits'.format(isim, ibin+1, cookie))
-            _cat = fits.open(filename)[1].data
+    # def _init_flask(self, isim, cookie):
+    #     for ibin in self.prog(self.zbins):
+    #         filename = os.path.join(self.data_dir, 'src-cat_s{}_z{}_ck{}.fits'.format(isim, ibin+1, cookie))
+    #         _cat = fits.open(filename)[1].data
 
+    #         cat = {}
+    #         cat['ra'] = _cat['RA']
+    #         cat['dec'] = _cat['DEC']
+
+    #         cat['e1'] = _cat['GAMMA1']
+    #         cat['e2'] = -1.0 * _cat['GAMMA2']
+
+    #         self.cats[ibin] = cat   
+
+    def split_bin(self, ibin, nsplits, remove=False):
+        full_cat = self.cats[ibin]
+
+        def get_splits(N, m):
+            # assign split indices with equal number of samples (or approximately if N%m!=0)
+            size, extra = divmod(N,m)
+            splits = np.concatenate([np.random.choice(m,size=extra,replace=False)]+[np.repeat(i,size) for i in range(m)])
+            np.random.shuffle(splits)
+            return splits
+
+        splits = get_splits(len(full_cat['ra']), nsplits)
+        for i in range(nsplits):
             cat = {}
-            cat['ra'] = _cat['RA']
-            cat['dec'] = _cat['DEC']
+            w = splits==i
+            cat['ra']  = full_cat['ra'][w]
+            cat['dec'] = full_cat['dec'][w]
+            cat['e1']  = full_cat['e1'][w]
+            cat['e2']  = full_cat['e2'][w]
+            if full_cat['weight'] is None:
+                cat['weight'] = None
+            else:
+                cat['weight']  = full_cat['weight'][w]
+            zbin = str(ibin)+'_'+str(i)
+            self.cats[zbin] = cat
+            self.zbins.append(zbin)
+            self.masks[zbin] = None
+            self.masks_apo[zbin] = None
+            self.maps[zbin] = {}
+            self.fields[zbin] = None
+            for name in self.map_names:
+                self.maps[zbin][name] = None
 
-            cat['e1'] = _cat['GAMMA1']
-            cat['e2'] = -1.0 * _cat['GAMMA2']
+        if remove:
+            self.zbins.remove(ibin)
+            self.cats.pop(ibin)
 
-            self.cats[ibin] = cat   
+        self.nzbins = len(self.zbins)
 
     def make_maps(self, save=True):
         keys = ['e1', 'e2']
         self.get_ipix()
         for ibin in self.prog(self.zbins, desc='{}.make_maps'.format(self.obs_name)):
             cat = self.cats[ibin]
-            quantities, count, mask = ca.cosmo.make_healpix_map(None, None,
+            quantities, count, mask, sum_w_maps = ca.cosmo.make_healpix_map(None, None,
                                                     quantity=[cat[_x] for _x in keys],
                                                     nside=self.nside, fill_UNSEEN=True,
-                                                    mask=None, weight=None,
-                                                    ipix=self.ipix[ibin])
+                                                    mask=None, weight=cat['weight'],
+                                                    ipix=self.ipix[ibin], return_w_maps=True,
+                                                    return_extra=False)
             for j, key in enumerate(keys):
                 self.maps[ibin][key] = quantities[j]
 
@@ -177,6 +248,7 @@ class Shear(Observable):
             count_cut_mask = (count>self.count_cut)
 
             self.maps[ibin]['count'] = count * count_cut_mask.astype(int)
+            self.maps[ibin]['weighted_count'] = sum_w_maps[0] * count_cut_mask.astype(int)
             self.masks[ibin] = mask.astype(float) * count_cut_mask.astype(float)
 
         if save:
@@ -188,7 +260,7 @@ class Shear(Observable):
         # Apply inverse-variance weighting after apodization
         if self.mask_mode =='count':
             for ibin in self.zbins:
-                self.masks_apo[ibin] *= self.maps[ibin]['count']
+                self.masks_apo[ibin] *= self.maps[ibin]['weighted_count']
 
     def prepare_fields(self):
         if self.fields_kw.get('purify_b', False) or self.fields_kw.get('purify_e', False):
@@ -202,6 +274,15 @@ class Shear(Observable):
             out[ibin] = [self.maps[ibin]['e1'], self.maps[ibin]['e2']]
         
         return out
+
+    def make_difference_field(self, ibin, jbin):
+        dbin = str(ibin)+'-'+str(jbin)
+
+        # Mask is intersection of both masks
+        mask = np.logical_and((self.masks[ibin]>0.), (self.masks[jbin]>0.)).astype(float)
+        mask_apo = nmt.mask_apodization(mask, aposize=self.aposize, apotype=self.apotype)
+
+        self.fields[dbin] = nmt.NmtField(mask_apo, [self.maps[ibin]['e1']-self.maps[jbin]['e1'], self.maps[ibin]['e2']-self.maps[jbin]['e2']], **self.fields_kw)
 
     # def make_fields(self, hm, include_templates=True):
     #     if hm.purify_b or hm.purify_e:
@@ -222,7 +303,7 @@ class Shear(Observable):
         bool_mask = (self.maps[ibin]['count'] > 0.)
         self.get_ipix()
 
-        e1_map, e2_map = _randrot_maps(self.cats[ibin]['e1'].astype(float), self.cats[ibin]['e2'].astype(float), self.ipix[ibin], self.npix, bool_mask, self.maps[ibin]['count'])
+        e1_map, e2_map = _randrot_maps(self.cats[ibin]['e1'].astype(float), self.cats[ibin]['e2'].astype(float), self.cats[ibin]['weight'], self.ipix[ibin], self.npix, bool_mask, self.maps[ibin]['weighted_count'])
 
         return [e1_map, e2_map]
     
@@ -425,7 +506,6 @@ class Shear(Observable):
             kappa[ibin]['E'][np.logical_not(self.masks[ibin].astype(bool))] = hp.UNSEEN
             kappa[ibin]['B'][np.logical_not(self.masks[ibin].astype(bool))] = hp.UNSEEN
 
-
         if return_alms:
             return kappa, kappa_alms
         else:
@@ -433,7 +513,20 @@ class Shear(Observable):
 
 
 @numba.jit(nopython=True, parallel=True)
-def _randrot_maps_sub(cat_e1, cat_e2, ipix, npix):
+def random_rotation(e1_in, e2_in):
+    n = len(e1_in)
+    e1_out = np.zeros(n)
+    e2_out = np.zeros(n)
+    for i in numba.prange(n):
+        rot_angle = random.random() * TWOPI
+        cos = np.cos(rot_angle)
+        sin = np.sin(rot_angle)
+        e1_out[i] = + e1_in[i] * cos + e2_in[i] * sin
+        e2_out[i] = - e1_in[i] * sin + e2_in[i] * cos
+    return e1_out, e2_out
+
+@numba.jit(nopython=True, parallel=True)
+def _randrot_maps_sub(cat_e1, cat_e2, w, ipix, npix):
     n = len(cat_e1)
     e1_map = np.zeros(npix)
     e2_map = np.zeros(npix)
@@ -441,16 +534,20 @@ def _randrot_maps_sub(cat_e1, cat_e2, ipix, npix):
         rot_angle = random.random() * TWOPI
         cos = np.cos(rot_angle)
         sin = np.sin(rot_angle)
-        e1_map[ipix[i]] += + cat_e1[i] * cos + cat_e2[i] * sin
-        e2_map[ipix[i]] += - cat_e1[i] * sin + cat_e2[i] * cos
+        if w is None:
+            e1_map[ipix[i]] += + cat_e1[i] * cos + cat_e2[i] * sin
+            e2_map[ipix[i]] += - cat_e1[i] * sin + cat_e2[i] * cos
+        else:
+            e1_map[ipix[i]] += w[i] * (+ cat_e1[i] * cos + cat_e2[i] * sin)
+            e2_map[ipix[i]] += w[i] * (- cat_e1[i] * sin + cat_e2[i] * cos)
     
     return e1_map, e2_map
 
-def _randrot_maps(cat_e1, cat_e2, ipix, npix, bool_mask, count):
-    e1_map, e2_map = _randrot_maps_sub(cat_e1, cat_e2, ipix, npix)
+def _randrot_maps(cat_e1, cat_e2, w, ipix, npix, bool_mask, weighted_count):
+    e1_map, e2_map = _randrot_maps_sub(cat_e1, cat_e2, w, ipix, npix)
 
-    e1_map[bool_mask] /= count[bool_mask]
-    e2_map[bool_mask] /= count[bool_mask]
+    e1_map[bool_mask] /= weighted_count[bool_mask]
+    e2_map[bool_mask] /= weighted_count[bool_mask]
 
     return e1_map, e2_map
 

harmony/harmony.py
@@ -7,7 +7,7 @@ import castor as ca
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import multiprocessing
-from .utils import *
+from .utils import prog, make_directory, compute_master
 from .observable import Observable
 import pickle
 import numpy as np
@@ -39,6 +39,7 @@ class Harmony(object):
             
         self.ell = self.b.get_effective_ells()
         self.cls = {}
+        self.cls['ell'] = self.ell
         self.wsp = {}
 
         self.nproc = nproc
@@ -46,6 +47,7 @@ class Harmony(object):
             self.pool = multiprocessing.Pool(nproc)
         
         make_directory(self.config.path_output+'/'+self.name)
+        make_directory(self.config.path_wsp+'/'+self.name)
 
         self.verbose = verbose
         self.prog = prog(verbose)
@@ -82,7 +84,7 @@ class Harmony(object):
             suffix = '{}_{}_{}_{}'.format(obs1, obs2, i1, i2)
         else:
             raise ValueError
-        filename = os.path.join(self.config.path_output, self.name, 'wsp_{}_nside{}_{}.bin'.format(self.config.name, self.nside, suffix))
+        filename = os.path.join(self.config.path_wsp, self.name, 'wsp_{}_nside{}_{}.bin'.format(self.config.name, self.nside, suffix))
         return filename
 
     def load_workspace(self, obs1, obs2, i1, i2):
@@ -107,12 +109,12 @@ class Harmony(object):
         else:
             raise RuntimeError("Workspace file does not exist: ", filename_bpws)
 
-    def load_all_workspaces(self, obs1, obs2=None):
-        pairs = self.get_pairs(obs1, obs2=obs2)
+    def load_all_workspaces(self, obs1, obs2=None, auto_only=False):
+        pairs = self.get_pairs(obs1, obs2=obs2, auto_only=auto_only)
         if obs2 is None:
             obs2 = obs1
 
-        for i1,i2 in self.prog(pairs, desc='Harmony.get_all_workspaces [obs1:{}, obs2={}]'.format(obs1.obs_name, obs2.obs_name)):
+        for i1,i2 in self.prog(pairs, desc='Harmony.load_all_workspaces [obs1:{}, obs2={}]'.format(obs1.obs_name, obs2.obs_name)):
             self.load_workspace(obs1, obs2, i1, i2)
         
     def _save_workspace(self, wsp, obs1, obs2, i1, i2, bpws_only=False):
@@ -121,14 +123,20 @@ class Harmony(object):
         if not bpws_only:
             wsp.write_to(filename)
 
-    def prepare_workspace(self, obs1, obs2, i1, i2, **kwargs):
+    def compute_workspace(self, obs1, obs2, i1, i2, wsp=None, **kwargs):
         self.check_obs(obs1, obs2)
 
         field1 = obs1.get_field(i1)
         field2 = obs2.get_field(i2)
 
-        wsp = nmt.NmtWorkspace()
+        if wsp is None:
+            wsp = nmt.NmtWorkspace()
         wsp.compute_coupling_matrix(field1, field2, self.b, **kwargs)
+        
+        return wsp
+
+    def prepare_workspace(self, obs1, obs2, i1, i2, **kwargs):
+        wsp = self.compute_workspace(obs1, obs2, i1, i2, **kwargs)
 
         self.wsp[(obs1.obs_name, obs2.obs_name)][(i1,i2)] = wsp
 
@@ -175,30 +183,37 @@ class Harmony(object):
             bpws = self.load_workspace_bpws(obs1, obs2, i1, i2)
             return bpws
 
-    def save_cls(self):
+    def save_cls(self, ext=None):
         make_directory(self.config.path_output+'/'+self.name)
         filename = os.path.join(self.config.path_output, self.name, 'cls_{}_nside{}.pickle'.format(self.config.name, self.nside))
+        if ext is not None:
+            filename = filename.replace('.pickle', '_'+str(ext)+'.pickle')
         pickle.dump(self.cls, open(filename, mode='wb'))
 
-    def load_cls(self, print_summary=True):
+    def load_cls(self, print_summary=True, ext=None, keep_cls=True):
         filename = os.path.join(self.config.path_output, self.name, 'cls_{}_nside{}.pickle'.format(self.config.name, self.nside))
-        self.cls = pickle.load(open(filename, mode='rb'))
+        if ext is not None:
+            filename = filename.replace('.pickle', '_'+str(ext)+'.pickle')
+        loaded_cls = pickle.load(open(filename, mode='rb'))
 
-        assert np.allclose(self.ell, self.cls['ell'])
+        if keep_cls:
+            assert np.allclose(self.ell, loaded_cls['ell'])
+            self.cls = loaded_cls
 
         if print_summary:
             print("Loaded Cl's info:")
-            print('  - Multipole bins ({}) = '.format(len(self.cls['ell'])), self.cls['ell'])
-            for obs_key in self.cls.keys():
+            print('  - Multipole bins ({}) = '.format(len(loaded_cls['ell'])), loaded_cls['ell'])
+            for obs_key in loaded_cls.keys():
                 if obs_key != 'ell':
                     print("  - Observables", obs_key)
-                    for bin_key in self.cls[obs_key].keys():
+                    for bin_key in loaded_cls[obs_key].keys():
                         print("     - Redshift bins", bin_key)
-                        for tr_key, cl in self.cls[obs_key][bin_key].items():
+                        for tr_key, cl in loaded_cls[obs_key][bin_key].items():
                             print("          -", tr_key, cl.shape)
-        return self.cls
 
-    def compute_cls(self, obs1, i1, obs2=None, i2=None, save_cls=None):
+        return loaded_cls
+
+    def compute_cls(self, obs1, i1, obs2=None, i2=None, save_cls=None, wsp=None):
         if obs2 is None:
             # same_obs = True
             obs2 = obs1
@@ -210,7 +225,8 @@ class Harmony(object):
         field1 = obs1.get_field(i1)
         field2 = obs2.get_field(i2)
 
-        wsp = self.get_workspace(obs1, obs2, i1, i2)
+        if wsp is None:
+            wsp = self.get_workspace(obs1, obs2, i1, i2)
 
         _cls = compute_master(field1, field2, wsp)
 
@@ -237,11 +253,11 @@ class Harmony(object):
         else:
             self.cls[(obs1.obs_name, obs2.obs_name)][(i1,i2)]['random'] = clr_new
 
-    def compute_random_cls(self, obs1, i1, obs2, i2, random_obs1, random_obs2, nrandom=1, auto_cls=False, save_cls=None):
+    def compute_random_cls(self, obs1, i1, obs2, i2, random_obs1, random_obs2, nrandom=1, auto_cls=False, save_cls=None, wsp=None, add_to_random=True):
         assert random_obs1 or random_obs2
         assert nrandom>0
         if auto_cls:
-            assert obs1==obs2
+            assert obs1==obs2 and i1==i2
 
         self.check_obs(obs1, obs2)
         if (i1, i2) not in self.cls[(obs1.obs_name, obs2.obs_name)].keys():
@@ -259,7 +275,8 @@ class Harmony(object):
         # else:
         #     fields2 = [obs2.get_field(self, i2)] * nrandom
 
-        wsp = self.get_workspace(obs1, obs2, i1, i2)
+        if wsp is None:
+            wsp = self.get_workspace(obs1, obs2, i1, i2)
 
         _cls = []
         for _ in self.prog(nrandom, desc='Harmony.compute_random_cls [obs1:{}, obs2={}]'.format(obs1.obs_name, obs2.obs_name)):
@@ -280,12 +297,15 @@ class Harmony(object):
 
             _cls.append(compute_master(field1, field2, wsp))
 
-        self._add_to_random(obs1, obs2, i1, i2, np.array(_cls))
+        if add_to_random:
+            self._add_to_random(obs1, obs2, i1, i2, np.array(_cls))
 
         if save_cls or self.do_save_cls:
             self.save_cls()
         
-        return self.cls[(obs1.obs_name, obs2.obs_name)][(i1,i2)]
+        # return self.cls[(obs1.obs_name, obs2.obs_name)][(i1,i2)]
+        return np.array(_cls)
+
 
     def compute_all_cls(self, obs1, obs2=None, save_cls=None, auto_only=False):
         pairs = self.get_pairs(obs1, obs2=obs2, auto_only=auto_only)
@@ -314,7 +334,7 @@ class Harmony(object):
                 self.cls[(obs1.obs_name, obs2.obs_name)][x] = {}
 
         if share_randoms:
-            for i in self.prog(nrandom, desc='Harmony.compute_random_all_cls [obs1:{}, obs2={}]'.format(obs1.obs_name, obs2.obs_name)):
+            for _ in self.prog(nrandom, desc='Harmony.compute_random_all_cls [obs1:{}, obs2={}]'.format(obs1.obs_name, obs2.obs_name)):
                 # Observable 1
                 if random_obs1:
                     fields1 = {i1:obs1.make_randomized_fields(i1, nrandom=1)[0] for i1 in pairs_i1}
@@ -389,8 +409,9 @@ class Harmony(object):
             self.compute_random_auto_cls(obs, nrandom=nrandom, save_cls=save_cls)
 
 
-    def bin_cl_theory(self, cl_in, obs1, obs2, i1, i2, fix_pixwin=False):
-        bpws = self.get_workspace_bpws(obs1, obs2, i1, i2)
+    def bin_cl_theory(self, cl_in, obs1, obs2, i1, i2, fix_pixwin=False, bpws=None):
+        if bpws is None:
+            bpws = self.get_workspace_bpws(obs1, obs2, i1, i2)
         if isinstance(cl_in, dict):
             cl = cl_in[(obs1.obs_name, obs2.obs_name)][(i1,i2)]
         else: